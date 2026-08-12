dc8fa80db84473a1195e67913b25d561eecda981
igsjena <dokedu@ericweber.de>
2026-08-12 12:57:17 +0200

    Update seed-admin.ts

--- scripts/seed-admin.ts
+++ scripts/seed-admin.ts
@@ -11,7 +11,7 @@ const client = new pg.Client({
   database: "postgres",
   user: process.env.DB_USER ?? "postgres",
   password: process.env.DB_PASSWORD,
-  ssl: true,
+  ssl: { rejectUnauthorized: false },
 })
 
 await client.connect()
